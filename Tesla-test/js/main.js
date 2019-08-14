180b78e361893eb2119ad74912d12e61e565b0d3
slavon4ik1 <m4ail@mail.ru>
2019-08-14 16:31:34 +0300

    add

---
+++ Tesla-test/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $('.next').click(function(){
+    $('.step_1').hide('slow', function() {
+    });
+    $('.step_2').show('slow', function() {
+      
+    });
+  });
+  $('.back').click(function() {
+    $('.step_2').hide('500', function() {
+      $('.step_1').show('500');
+    });
+  });
+  $('.next_2').click(function() {
+    $('.step_2').hide('slow', function() {  
+    });
+    $('.step_3').show((500), function() {
+     setTimeout(function() { $(".step_3").hide('slow'); }, 3000);
+     setTimeout(function() { $(".step_4").show('slow'); }, 3000);
+    });
+  });
+
+  
+
+
+
+});

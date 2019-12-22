ee757090446262c74af05436390b369dbc7fea5c
Slava <m4ail@mail.ru>
2019-12-22 11:02:56 +0200

    add files

---
+++ tremainartaza/precache-manifest.da301e19f0306cf51a0c310734969860.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4847c68aa9c204c86c7ff81c083262dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aabacc57ea4116443d5",
+    "url": "/static/css/main.4088a577.chunk.css"
+  },
+  {
+    "revision": "50b1282ef2db05d72762",
+    "url": "/static/js/2.acc5670a.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.acc5670a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3aabacc57ea4116443d5",
+    "url": "/static/js/main.4450af20.chunk.js"
+  },
+  {
+    "revision": "ccbecf878d65eee8a036",
+    "url": "/static/js/runtime-main.fa8d3903.js"
+  },
+  {
+    "revision": "1fc524a22c99e8d63393ecfe238e3d35",
+    "url": "/static/media/ProximaNova-Regular.1fc524a2.ttf"
+  },
+  {
+    "revision": "a8ea564afc114e1a061cb9ef27a0560b",
+    "url": "/static/media/ProximaNova-Regular.a8ea564a.woff"
+  },
+  {
+    "revision": "eaea72f6b018036dacb0c398d60d82c8",
+    "url": "/static/media/ProximaNova-Regular.eaea72f6.eot"
+  },
+  {
+    "revision": "ecef0faef83874e9f6bb9b5e9fa05a0e",
+    "url": "/static/media/ProximaNova-Regular.ecef0fae.woff2"
+  },
+  {
+    "revision": "92f22d8e110e57d9751a9b472afca4cd",
+    "url": "/static/media/image-bg.92f22d8e.jpeg"
+  }
+]);

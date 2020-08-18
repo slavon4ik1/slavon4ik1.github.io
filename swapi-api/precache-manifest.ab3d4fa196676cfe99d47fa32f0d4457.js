3a4832c6887645178158711785c84de17e62ce7f
Slava <m4ail@mail.ru>
2020-08-18 21:06:28 +0300

    add swapi

---
+++ swapi-api/precache-manifest.ab3d4fa196676cfe99d47fa32f0d4457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381d805fbd60a717a4fdc39c20a727e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b131be6c4a45b05c0ede",
+    "url": "/static/css/main.543ce97f.chunk.css"
+  },
+  {
+    "revision": "1602fe1b6c9cd612bffc",
+    "url": "/static/js/2.24c6a773.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.24c6a773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b131be6c4a45b05c0ede",
+    "url": "/static/js/main.ccad7d82.chunk.js"
+  },
+  {
+    "revision": "4b34dda6bfa1891d0371",
+    "url": "/static/js/runtime-main.7a3a587e.js"
+  }
+]);

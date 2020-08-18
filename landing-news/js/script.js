12bcc075278dce760f54a134a23c939410b74a27
Slava <m4ail@mail.ru>
2020-08-18 20:34:06 +0300

    some changes
    some changes

--- landing-news/js/script.js
+++
@@ -1,8 +0,0 @@
-(function ($) {  
-
-  $(".header-menu__btn").click(function() {
-    $(this).toggleClass("active");
-    $(".header-menu").toggleClass("active");
-  });
-
-})(jQuery);
---
+++ todo-list-react/precache-manifest.b5c9eeb71f5501f90bdb16fd216ecddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04ce5b8413da485e5d5d95bd4b39315",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b71fec8608826932ba91",
+    "url": "/static/css/main.ab606250.chunk.css"
+  },
+  {
+    "revision": "be19641d3fe78d35223f",
+    "url": "/static/js/2.ef8b4283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef8b4283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71fec8608826932ba91",
+    "url": "/static/js/main.821aed6c.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);

ed4e6bb7392bed662a0f78bbd45e1a6f6a9e31e2
Slava <m4ail@mail.ru>
2020-09-06 15:08:00 +0300

    test work

---
+++ luxestate/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        700:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:false,
+            loop:true
+        }
+    }
+  })
+});
+

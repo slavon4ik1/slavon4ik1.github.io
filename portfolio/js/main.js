8b09558ba120e0b2b014e9f5573d13093637a282
Slava <m4ail@mail.ru>
2020-12-18 19:53:51 +0200

    del

--- portfolio/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('a[href^="#"]').click(function(){
-       var target = $(this).attr('href');
-       $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
-       return false;
-    });
-  });
